4eeafa56df1b94c39630516cdb632fc56f006070
Denis Scheglov <dscheglov@ptsecurity.com>
2018-04-29 17:46:13 +0300

    add GetTx() returns statement to be executed linked to provided transaction

--- preparedsqlx.go
+++ preparedsqlx.go
@@ -15,11 +15,13 @@ func Add(name, query string) {
 
 // Registry keeps prepared statements linked to db for each query of global queryRegistry
 type Registry struct {
+	db *sqlx.DB
 	storage map[string]*sqlx.Stmt
 }
 
+// New prepares all queries listed in registry to provided DB and returns Registry instance
 func New(db *sqlx.DB) (*Registry, error) {
-	registry := &Registry{storage: make(map[string]*sqlx.Stmt, len(queryRegistry))}
+	registry := &Registry{db: db, storage: make(map[string]*sqlx.Stmt, len(queryRegistry))}
 	err := registry.Prepare(db)
 	if err != nil {
 		return nil, err
@@ -27,6 +29,7 @@ func New(db *sqlx.DB) (*Registry, error) {
 	return registry, nil
 }
 
+// Prepare all queries listed in registry to provided DB
 func (r *Registry) Prepare(db *sqlx.DB) (err error) {
 	for name, query := range queryRegistry {
 		r.storage[name], err = db.Preparex(query)
@@ -37,12 +40,26 @@ func (r *Registry) Prepare(db *sqlx.DB) (err error) {
 	return nil
 }
 
+// Get returns statement to be executed
 func (r *Registry) Get(query string) (*sqlx.Stmt, error) {
 	if r.storage[query] == nil {
 		if queryRegistry[query] == "" {
 			return nil, fmt.Errorf("prepared query '%s' is not added", query)
 		}
-		return nil, fmt.Errorf("query '%s' is not prepared", query)
+		stmt, err := r.db.Preparex(queryRegistry[query])
+		if err != nil {
+			return nil, err
+		}
+		r.storage[query] = stmt
 	}
 	return r.storage[query], nil
+}
+
+// GetTx returns statement to be executed linked to provided transaction
+func (r *Registry) GetTx(tx sqlx.Tx, query string) (*sqlx.Stmt, error) {
+	pg, err := r.Get(query)
+	if err != nil {
+		return nil, err
+	}
+	return tx.Stmtx(pg), nil
 }
